9f9de4ddd5a7ceb8c0380ab79228ba7a088c38d1
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2023-06-08 16:25:55 +0200

    Fix wrong merge confilct resolution

--- lua/neoai/chat/models/openai.lua
+++ lua/neoai/chat/models/openai.lua
@@ -18,51 +18,51 @@ local raw_chunks = {}
 
 ---@brief Cancel the current stream and shut down the handler
 M.cancel_stream = function()
-  if handler ~= nil then
-    handler:shutdown()
-    handler = nil
-  end
+    if handler ~= nil then
+        handler:shutdown()
+        handler = nil
+    end
 end
 
 M.get_current_output = function()
-	return table.concat(chunks, "")
+    return table.concat(chunks, "")
 end
 
 ---@param chunk string
 ---@param on_stdout_chunk fun(chunk: string) Function to call whenever a stdout chunk occurs
 M._receive_chunk = function(chunk, on_stdout_chunk)
-	local function safely_extract_delta_content(decoded_json)
-		local path = decoded_json.choices
-		if not path then
-			return nil
-		end
+    local function safely_extract_delta_content(decoded_json)
+        local path = decoded_json.choices
+        if not path then
+            return nil
+        end
 
-		path = path[1]
-		if not path then
-			return nil
-		end
+        path = path[1]
+        if not path then
+            return nil
+        end
 
-		path = path.delta
-		if not path then
-			return nil
-		end
+        path = path.delta
+        if not path then
+            return nil
+        end
 
-		return path.content
-	end
-	-- Remove "data:" prefix from chunk
-	local raw_json = string.gsub(chunk, "%s*data:%s*", "")
-	table.insert(raw_chunks, raw_json)
+        return path.content
+    end
+    -- Remove "data:" prefix from chunk
+    local raw_json = string.gsub(chunk, "%s*data:%s*", "")
+    table.insert(raw_chunks, raw_json)
 
-	local ok, decoded_json = pcall(vim.json.decode, raw_json)
-	if not ok then
-		return -- Ignore invalid JSON chunks
-	end
+    local ok, decoded_json = pcall(vim.json.decode, raw_json)
+    if not ok then
+        return -- Ignore invalid JSON chunks
+    end
 
-	local delta_content = safely_extract_delta_content(decoded_json)
-	if delta_content then
-		table.insert(chunks, delta_content)
-		on_stdout_chunk(delta_content)
-	end
+    local delta_content = safely_extract_delta_content(decoded_json)
+    if delta_content then
+        table.insert(chunks, delta_content)
+        on_stdout_chunk(delta_content)
+    end
 end
 
 ---@param chat_history ChatHistory
@@ -71,41 +71,42 @@ end
 M.send_to_model = function(chat_history, on_stdout_chunk, on_complete)
     local api_key = config.options.open_ai.api_key.get()
 
-	local data = {
-		model = chat_history.model,
-		stream = true,
-		messages = chat_history.messages,
-	}
-	data = vim.tbl_deep_extend("force", {}, data, chat_history.params)
+    local data = {
+        model = chat_history.model,
+        stream = true,
+        messages = chat_history.messages,
+    }
+    data = vim.tbl_deep_extend("force", {}, data, chat_history.params)
 
     chunks = {}
     raw_chunks = {}
     log.debug("Sending to OpenAI: " .. vim.inspect(data))
-    utils.exec("curl", {
-        "--silent",
-        "--show-error",
-        "--no-buffer",
-        "https://api.openai.com/v1/chat/completions",
-        "-H",
-        "Content-Type: application/json",
-        "-H",
-        "Authorization: Bearer " .. api_key,
-        "-d",
-        vim.json.encode(data),
-    }, function(chunk)
-        M._recieve_chunk(chunk, on_stdout_chunk)
-    end, function(err, _)
-        local total_message = table.concat(raw_chunks, "")
-        local ok, json = pcall(vim.json.decode, total_message)
-        if ok then
-            if json.error ~= nil then
-                on_complete(json.error.message, nil)
-                log.error(json.error)
-                return
+    handler = curl.post({
+        url = "https://api.openai.com/v1/chat/completions",
+        raw = { "--no-buffer" },
+        headers = {
+            content_type = "application/json",
+            Authorization = "Bearer " .. api_key,
+        },
+        body = vim.json.encode(data),
+        stream = function(_, chunk)
+            if chunk ~= "" then
+                -- The following workaround helps to identify when the model has completed its task.
+                if string.match(chunk, "%[DONE%]") then
+                    vim.schedule(function()
+                        on_complete(nil, M.get_current_output())
+                    end)
+                else
+                    vim.schedule(function()
+                        M._receive_chunk(chunk, on_stdout_chunk)
+                    end)
+                end
             end
-        end
-        on_complete(err, M.get_current_output())
-    end)
+        end,
+        on_error = function(err, _, _)
+            return on_complete(err, nil)
+        end,
+    })
 end
 
 return M
